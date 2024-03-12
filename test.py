2ecae60f949474def149b42724743a68b54abda1
lmonsalve22 <lmonsalve22@gmail.com>
2024-03-12 15:57:59 -0300

    primer commit

--- test.py
+++ test.py
@@ -1,127 +1,65 @@
 import pandas as pd
-from datetime import datetime
+import http.client
+import zipfile
 
-date_format = "%Y/%m/%d"
+def getZIP():
+    # Definir los parámetros de la solicitud
+    host = "repositoriodeis.minsal.cl"
+    path = "/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2021_2024_12032024.zip"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
+    headers = {
+        "Host": host,
+        "User-Agent": user_agent,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "es-CL,es;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
+        "TE": "trailers",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"
+    }
 
-columnas_deseadas = ['organismo_nombre', 'organismo_codigo', 'fecha_publicacion_ta', 'anyo',
-       'Mes']
+    # Establecer la conexión con el servidor
+    conn = http.client.HTTPSConnection(host)
 
-columnas_deseadas2 = ['organismo_nombre', 'organismo_codigo', 'fecha_publicacion', 'anyo',
-       'Mes']
+    # Realizar la solicitud GET
+    conn.request("GET", path, headers=headers)
 
-base = "https://www.cplt.cl/transparencia_activa/datoabierto/archivos/"
-TA_PasivosMunicipio                     = f"{base}TA_PasivosMunicipio.csv"
-TA_ActosDocPublicadosenDO               = f"{base}TA_ActosDocPublicadosenDO.csv"
-TA_Potestades_otras                     = f"{base}TA_Potestades_otras.csv"
-TA_Marco_normativo                      = f"{base}TA_Marco_normativo.csv"
-TA_Facultades_funciones_atribuciones    = f"{base}TA_Facultades_funciones_atribuciones.csv"
-TA_Otras_transferencias                 = f"{base}TA_Otras_transferencias.csv"
-TA_Tramites_ante_consejo                = f"{base}TA_Tramites_ante_consejo.csv"
-TA_ParticipacionCiudadana               = f"{base}TA_ParticipacionCiudadana.csv"
-TA_Auditorias                           = f"{base}TA_Auditorias.csv"
-TA_Subsidios_beneficios_intermediarios  = f"{base}TA_Subsidios_beneficios_intermediarios.csv"
-TA_Subsidios_beneficios                 = f"{base}TA_Subsidios_beneficios.csv"
-TA_PersonalPlanta                       = f"{base}TA_PersonalPlanta.csv"
-TA_PersonalContrata                     = f"{base}TA_PersonalContrata.csv"
-TA_PersonalCodigotrabajo                = f"{base}TA_PersonalCodigotrabajo.csv"
-TA_PersonalContratohonorarios           = f"{base}TA_PersonalContratohonorarios.csv"
-TA_Otras_compras                        = f"{base}TA_Otras_compras.csv"
-TA_Otras_autoridades                    = f"{base}TA_Otras_autoridades.csv"
-TA_Nomina_beneficiarios                 = f"{base}TA_Nomina_beneficiarios.csv"
-TA_Licitaciones                         = f"{base}TA_Licitaciones.csv"
+    # Obtener la respuesta
+    response = conn.getresponse()
 
-def getDF(url):
-    return pd.read_csv(url,sep=";",encoding="latin")
+    # Leer el contenido de la respuesta
+    if response.status == 200:
+        with open("descarga.zip", "wb") as f:
+            f.write(response.read())
+        print("Archivo descargado exitosamente.")
+    else:
+        print("Error al descargar el archivo:", response.status)
 
+    # Cerrar la conexión
+    conn.close()
 
-parte1 = [TA_PasivosMunicipio                      ,
-                TA_ActosDocPublicadosenDO               ,
-                TA_Potestades_otras                     ,
-                TA_Marco_normativo                      ,
-                TA_Facultades_funciones_atribuciones    ,
-                TA_Otras_transferencias                 ,
-                TA_Tramites_ante_consejo                ,
-                TA_ParticipacionCiudadana               ,
-                TA_Auditorias                           ,
-                TA_Subsidios_beneficios_intermediarios  ,
-                TA_Subsidios_beneficios                 ,
-                #TA_PersonalPlanta                       ,
-                #TA_PersonalContrata                     ,
-                #TA_PersonalCodigotrabajo                ,
-                #TA_PersonalContratohonorarios           ,
-                #TA_Otras_compras                        ,
-                #TA_Otras_autoridades                    ,
-                #TA_Nomina_beneficiarios                 ,
-                #TA_Licitaciones
-                ]
-parte2 = [TA_PersonalPlanta]
+def descomprimir():
+    # Ruta del archivo ZIP
+    archivo_zip = "descarga.zip"
 
+    # Abre el archivo ZIP en modo lectura
+    with zipfile.ZipFile(archivo_zip, 'r') as zip_ref:
+        # Lista los contenidos del archivo ZIP
+        contenidos = zip_ref.namelist()
 
-def descarga():
-    salida = []
-    for url in parte1:              
-        df = pd.read_csv(url, sep=";", encoding="latin")        
-        nombreArchivo = url.replace(base,"").replace(".csv","")        
-        df["Archivo"] = nombreArchivo
-        #print(df.shape)
-        print(url,len(df))
-        salida.append(df.copy())
-    return salida
+        # Extrae todos los archivos del ZIP en la carpeta actual
+        zip_ref.extractall()
 
-def getFecha(fecha):
-    try:
-        return datetime.strptime(fecha, date_format)
-    except:
-        return None
-
-def consolidar():
-    lista = descarga()
-
-    consolidador = []
-    for df in lista:
-        acumulador = []
-        diccionario = {}
-        diccionario["Archivo"] = df.iloc[0]["Archivo"]        
-        diccionario["Institucion"] = "No Institucion"
-        diccionario["Última Actualización"] = ""
-        for mes in ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]:
-            diccionario[mes] = 0
-        diccionario["Sin Año-Mes"] = 0
-        diccionario["Sin Mes"] = 0
-        diccionario["Sin Año"] = 0
-        diccionario["Total"] = 0
-        acumulador.append(diccionario.copy())
-        lista_organismo = list(df['organismo_nombre'].unique())
-        for anyo in [2022,2023,2024]:
-            df2 = df[df["anyo"] == anyo]
-            #for institucion in df2['organismo_nombre'].unique():
-            for institucion in lista_organismo: 
-                dfIntitucion = df2[df2['organismo_nombre'] == institucion]
-                diccionario = {}
-                diccionario["Archivo"] = df.iloc[0]["Archivo"]
-                try:
-                    diccionario["Última Actualización"] = dfIntitucion["fecha_publicacion_ta"].max()
-                except:
-                    diccionario["Última Actualización"] = dfIntitucion["fecha_publicacion"].max()
-                diccionario["Institucion"] = institucion
-                #diccionario["Codigo"] = dfIntitucion.iloc[0]["organismo_codigo"]
-                diccionario["Codigo"] = df[df['organismo_nombre'] == institucion].iloc[0]["organismo_codigo"]
-                diccionario["Sin Año-Mes"] = len(dfIntitucion.query('Mes.isnull() and anyo.isnull()'))
-                for mes in dfIntitucion["Mes"].unique():
-                    diccionario[mes] = "x"
-                diccionario["Sin Mes"] = len(dfIntitucion.query('Mes.isnull() and anyo.notnull()'))
-                diccionario["Sin Año"] = len(dfIntitucion.query('Mes.notnull() and anyo.isnull()'))
-                diccionario["Total"] = len(dfIntitucion)    
-                diccionario["año_auditoria"] = anyo
-                acumulador.append(diccionario.copy())
-        salida = pd.DataFrame(acumulador)
-        salida2 = salida[salida["Institucion"] != "No Institucion"]
-        #salida2["Fecha"] = salida2["Última Actualización"].apply(lambda x:datetime.strptime(x, date_format))
-        salida2["Fecha"] = salida2["Última Actualización"].apply(getFecha)
-        consolidador.append(salida2.copy())
-    final = pd.concat(consolidador)   
-    final.to_excel("consolidado.xlsx", index=False)
 
 
 if __name__ == '__main__':
-    consolidar()
+    getZIP()
+    descomprimir()
+    df = pd.read_csv("DEFUNCIONES_FUENTE_DEIS_2021_2024_12032024.csv", encoding='latin-1',sep=";", header=None)
+    df.to_excel("difuntos.xlsx",index=False)
