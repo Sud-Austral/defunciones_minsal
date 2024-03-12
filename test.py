67327aa9d06c2ee23386cb2c2969ce2c61338376
lmonsalve22 <lmonsalve22@gmail.com>
2024-03-12 16:53:22 -0300

    lo mismo con request

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import http.client
 import zipfile
 import time
+import requests
 
 def getZIP():
     # Definir los parámetros de la solicitud
@@ -9,7 +10,6 @@ def getZIP():
     path = "/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2021_2024_12032024.zip"
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
     headers = {
-        "Host": host,
         "User-Agent": user_agent,
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "es-CL,es;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -24,62 +24,27 @@ def getZIP():
         "Pragma": "no-cache",
         "Cache-Control": "no-cache"
     }
-    """
-    # Establecer la conexión con el servidor
-    conn = http.client.HTTPSConnection(host, timeout=1000)
-    # Realizar la solicitud GET
-    conn.request("GET", path, headers=headers)
-    # Obtener la respuesta
-    response = conn.getresponse()
-    # Leer el contenido de la respuesta
-    if response.status == 200:
-        with open("descarga.zip", "wb") as f:
-            f.write(response.read())
-        print("Archivo descargado exitosamente.")
-    #else:
-    #    print("Error al descargar el archivo:", response.status)
-    # Cerrar la conexión
-    #conn.close()
-    """
-    for intento in range(3):  # Realizar hasta 3 intentos
-        try:
-            # Establecer la conexión con el servidor y configurar el tiempo de espera
-            proxy_host = '45.225.207.186'
-            proxy_port = 999  # Reemplazar con el puerto de tu proxy
-            proxy_timeout = 1000  # Tiempo de espera en segundos
 
-            # Establecer la conexión con el servidor utilizando el proxy y el timeout
-            conn = http.client.HTTPSConnection(proxy_host, proxy_port, timeout=proxy_timeout)
-            conn = http.client.HTTPSConnection(host, timeout=1000)  # Tiempo de espera de 10 segundos
-            # Realizar la solicitud GET
-            conn.request("GET", path, headers=headers)
-            # Obtener la respuesta
-            response = conn.getresponse()
-            # Leer el contenido de la respuesta, manejarla como sea necesario
-            if response.status == 200:
-                with open("descarga.zip", "wb") as f:
-                    f.write(response.read())
-                print("Archivo descargado exitosamente.")
-            else:
-                print("Error al descargar el archivo:", response.status)
-            # Cerrar la conexión
-            conn.close()
-            # Si la conexión y la solicitud tienen éxito, salir del bucle de intentos
-            break
+    # Configurar el tiempo de espera
+    timeout = 100  # Tiempo de espera en segundos
 
-        except TimeoutError:
-            # Si ocurre un error de tiempo de espera, esperar unos segundos antes de volver a intentarlo
-            print("Tiempo de espera agotado. Reintentando conexión...")
-            print(TimeoutError)
-            time.sleep(5)  # Esperar 5 segundos antes de volver a intentarlo
-            continue
+    try:
+        # Realizar la solicitud GET con timeout
+        response = requests.get(f"https://{host}{path}", headers=headers, timeout=timeout)
 
-        except Exception as e:
-            print("Error:", e)
-            break
+        # Verificar el código de estado de la respuesta
+        if response.status_code == 200:
+            with open("descarga4.zip", "wb") as f:
+                f.write(response.content)
+            print("Archivo descargado exitosamente.")
+        else:
+            print("Error al descargar el archivo:", response.status_code)
 
-    else:
-        print("Se superó el número máximo de intentos. No se pudo establecer la conexión.")
+    except requests.Timeout:
+        print("Se ha agotado el tiempo de espera durante la solicitud.")
+
+    except requests.RequestException as e:
+        print("Error durante la solicitud:", e)
 
 
         
