9b978dd0bf22aa61a60493369a61dbdd8ea7d87a
lmonsalve22 <lmonsalve22@gmail.com>
2024-03-19 16:41:48 -0300

    gecko2

--- gecko.py
+++ gecko.py
@@ -2,17 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 
-print("Termino")
-url = "https://repositoriodeis.minsal.cl/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2021_2024_19032024.zip"
 
-options = Options()
-options.add_argument('-headless')
 
-# Iniciar el navegador Firefox
-driver = webdriver.Firefox(options=options)
-#driver.implicitly_wait(3000000)
-# Abrir la página web
-try:
-    driver.get(url)
-except:
-    print("No se pero llego aca")
+if __name__ == '__main__':
+    print("Termino")
+    url = "https://repositoriodeis.minsal.cl/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2021_2024_19032024.zip"
+
+    options = Options()
+    options.add_argument('-headless')
+
+    # Iniciar el navegador Firefox
+    driver = webdriver.Firefox(options=options)
+    #driver.implicitly_wait(3000000)
+    # Abrir la página web
+    try:
+        driver.get(url)
+    except:
+        print("No se pero llego aca")
